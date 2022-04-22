16f43484989f2581285fd36b586ee5bd7f542e37
WoosubLeee <zbxv1423@naver.com>
2022-04-23 00:42:51 +0900

    deployment 브랜치 환경변수 생성 함수 변경

--- woochelinsguide/env.py
+++ woochelinsguide/env.py
@@ -2,15 +2,15 @@ from decouple import config
 
 
 def get_env_vars():
-    config_allowed_hosts = config('ALLOWED_HOSTS_DEV')
+    config_allowed_hosts = config('ALLOWED_HOSTS_PROD')
     if config_allowed_hosts != 'None':
-        ALLOWED_HOSTS_DEV = config_allowed_hosts.split(', ')
+        ALLOWED_HOSTS_PROD = config_allowed_hosts.split(', ')
     else:
-        ALLOWED_HOSTS_DEV = []
+        ALLOWED_HOSTS_PROD = []
 
     return {
-        'SECRET_KEY': config('SECRET_KEY_DEV'),
+        'SECRET_KEY': config('SECRET_KEY_PROD'),
         'DEBUG': True,
-        'ALLOWED_HOSTS': ALLOWED_HOSTS_DEV,
-        'CORS_ALLOWED_ORIGINS': config('CORS_ALLOWED_ORIGINS_DEV').split(', '),
+        'ALLOWED_HOSTS': ALLOWED_HOSTS_PROD,
+        'CORS_ALLOWED_ORIGINS': config('CORS_ALLOWED_ORIGINS_PROD').split(', '),
     }
